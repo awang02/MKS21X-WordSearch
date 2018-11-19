4c6cf745736e8113cda9219b0b255c08e7bf4c43
Allen Wang <awang02@stuy.edu>
2018-11-19 05:57:23 -0500

    Completed addword and addallwords methods

--- WordSearch.java
+++ WordSearch.java
@@ -89,17 +89,102 @@ public class WordSearch{
    */
   public String toString(){
     String grid = new String("");
-    for (int r = 0; r < row; r++){
-      grid += "|"
-      for (int c = 0; c < col; c++){
-        grid += data[r][c] + " ";
+    for (int r = 0; r < data.length; r++){
+      grid += "|";
+      for (int c = 0; c < data[r].length; c++){
+        grid += data[r][c];
+        if (!(r == data[r].length - 1)){
+          grid += ",";
+        }
       }
-      grid -= " ";
       grid += "|\n";
     }
+
+    grid += "Words: ";
+    for (int i = 0; i < wordsAdded.size(); i++){
+      grid += wordsAdded.get(i);
+      if (!(i == data[i].length - 1)){
+        grid += " ";
+      }
+    }
+    grid += "\n";
+
+    grid += "Seed: " + seed;
     return grid;
   }
 
+  /**Attempts to add a given word to the specified position of the WordGrid.
+   *The word is added in the direction rowIncrement,colIncrement
+   *Words must have a corresponding letter to match any letters that it overlaps.
+   *
+   *@param word is any text to be added to the word grid.
+   *@param row is the vertical locaiton of where you want the word to start.
+   *@param col is the horizontal location of where you want the word to start.
+   *@param rowIncrement is -1,0, or 1 and represents the displacement of each letter in the row direction
+   *@param colIncrement is -1,0, or 1 and represents the displacement of each letter in the col direction
+   *@return true when: the word is added successfully.
+   *        false when: the word doesn't fit, OR  rowchange and colchange are both 0,
+   *        OR there are overlapping letters that do not match
+   */
+   private boolean addWord(String word,int row, int col, int rowIncrement, int colIncrement){
+     try{
+       if (row < 0 || col < 0 || col > data[row].length || row > data.length) {
+         return false;
+       }
+       if (rowIncrement == 0 && colIncrement == 0){
+         return false;
+       }
+       if (rowIncrement != -1 && rowIncrement != 0 && rowIncrement != 1)) {
+         return false;
+       }
+       if (colIncrement != -1 && colIncrement != 0 && colIncrement != 1)) {
+         return false;
+       }
+       for (int i = 0; i < word.length(); i++){
+           if ((data[row + i * rowIncrement][col + i * colIncrement] != '_') && (data[row + i * rowIncrement][col + i * colIncrement != word.charAt(i))){
+             return false;
+           }
+       }
+       // to avoid adding failed words
+       for (int i = 0; i < word.length(); i++){
+           data[row + i * rowIncrement][col + i * colIncrement
+       }
+       return true;
+     }catch(ArrayIndexOutOfBoundsException e){
+       return false;
+     }
+   }
+    /*[rowIncrement,colIncrement] examples:
+     *[-1,1] would add up and the right because (row -1 each time, col + 1 each time)
+     *[ 1,0] would add downwards because (row+1), with no col change
+     *[ 0,-1] would add towards the left because (col - 1), with no row change
+     */
+
+
+     private void addAllWords() {
+       int row;
+       int col;
+       int rowIncrement;
+       int colIncrement;
+       int attempts = 0;
+       int max = 150;
+       for (int i = 0; i < wordsToAdd.size() - 1; i++){
+         while (attempts < max){
+           row = Math.abs(randgen.nextInt(seed) % data.length - 1);
+           col = Math.abs(randgen.nextInt(seed) % data[0].length - 1);
+           rowIncrement = Math.abs(randgen.nextInt(2));
+           colIncrement = Math.abs(randgen.nextInt(2));
+           if (AddWord(wordsToAdd.get(i), row, col, rowIncrement, colIncrement)){
+             attempts = max;
+             String s = wordsToAdd.get(i);
+             wordsToAdd.remove(i);
+             wordsAdded.add(s);
+           } else {
+             attempts++;
+           }
+         }
+       }
+     }
   /**Attempts to add a given word to the specified position of the WordGrid.
    *The word is added from left to right, must fit on the WordGrid, and must
    *have a corresponding letter to match any letters that it overlaps.
